0feb10e33ce461c49b5f98756b3f42c5322eb137
Andre Haas <andre@andrehaas.com>
2020-03-07 15:35:27 -0800

    vis2: Set up basic table visualization.

--- goty.js
+++ goty.js
@@ -2,6 +2,9 @@ GOTY = {};
 
 (function() {
 
+const MIN_YEAR = 2000;
+const MAX_YEAR = 2019;
+
 const prepData = () => {
 
   let gamesByYear = d3.nest()
@@ -34,7 +37,7 @@ const prepData = () => {
     }
 
     gamesByYear[year] = {
-      max: gamesByYear[year].slice(0, i),
+      topGames: gamesByYear[year].slice(0, i),
       runnersUp: gamesByYear[year].slice(i, j),
     };
   });
@@ -43,19 +46,51 @@ const prepData = () => {
 
 };
 
+// not in use
+const getMaxTopGameOwners = (gamesByYear) => {
+  let out = 0;
+  for (let year=MIN_YEAR; year <= MAX_YEAR; year++) {
+    const yearOwners = getTopGameOwners(gamesByYear, year);
+    if (yearOwners > out) {
+      out = yearOwners;
+    }
+  }
+  return out;
+}
+
+// not in use
+const getTopGameOwners = (gamesByYear, year) => {
+  let out = 0;
+  gamesByYear[year].topGames.forEach(g => {
+    out += g.owners;
+  });
+  return out;
+}
+
 GOTY.init = () => {
 
   const gamesByYear = prepData();
   console.log(gamesByYear);
   const container = document.getElementById('vis2');
-  for (let i=2000; i <= 2019; i++) {
-    const max = gamesByYear[i].max[0].name;
+  // const maxTopGameOwners = getMaxTopGameOwners(gamesByYear);
+  for (let i=MIN_YEAR; i <= MAX_YEAR; i++) {
     container.innerHTML += `<div data-year="${i}" class="goty-year"></div>`
   }
   Array.from(document.getElementsByClassName('goty-year'))
       .forEach(div => {
-        const year = div.getAttribute('data-year')
-        div.innerHTML = `<div class="year-label">${year}</div>`;
+        const year = div.getAttribute('data-year');
+        // const width = getTopGameOwners(gamesByYear, year) / maxTopGameOwners * 100;
+        width = 100;
+        html = ''
+        html += `<div class="year-label">${year}</div>`;
+        html += `<div class="games-container">`;
+        html += `<div class="games-bar" style="width: ${width}%;">`;
+        gamesByYear[year].topGames.forEach(g => {
+          html += `<div class="top-game">${g.name}</div>`;
+        });
+        html += `</div>`
+        html += `</div>`
+        div.innerHTML = html;
       });
 
 }
